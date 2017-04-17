d09b332d9fcd5a051991ba98e4915733ea92e42e
Wilson Guimarães <wegneto@gmail.com>
2017-04-17 22:45:53 +0100

    Lecture 4. require and module.exports

---
+++ MEAN/app.js
@@ -0,0 +1,12 @@
+require('./instantHello');
+var goodbye = require('./talk/goodbye');
+var talk = require('./talk');
+var question = require('./talk/question');
+
+talk.intro();
+talk.hello("Wilson");
+
+var answer = question.ask("What is the meaning of life?");
+console.log(answer);
+
+goodbye();
