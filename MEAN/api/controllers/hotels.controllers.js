235c0d74cd8265d299b09362959ef7e60adb28ec
Wilson Guimarães <wegneto@gmail.com>
2017-04-23 16:53:29 +0100

    Lecture 23: MongoDB - Creating documents from Node.js

--- MEAN/api/controllers/hotels.controllers.js
+++ MEAN/api/controllers/hotels.controllers.js
@@ -1,5 +1,4 @@
 var dbconn = require('../data/dbconnection.js');
-var hotelData = require('../data/hotel-data.json');
 var ObjectId = require('mongodb').ObjectId;
 
 module.exports.hotelsGetAll = function (req, res) {
@@ -21,10 +20,10 @@ module.exports.hotelsGetAll = function (req, res) {
         .find()
         .skip(offset)
         .limit(count)
-        .toArray(function(err, docs) {
-        console.log("Found hotels", docs);
-        res.status(200).json(docs);
-    });
+        .toArray(function (err, docs) {
+            console.log("Found hotels", docs);
+            res.status(200).json(docs);
+        });
 };
 
 module.exports.hotelsGetOne = function (req, res) {
@@ -34,18 +33,32 @@ module.exports.hotelsGetOne = function (req, res) {
     var hotelId = req.params.hotelId;
     console.log("GET hotelID", hotelId);
 
-    collection 
+    collection
         .findOne({
-            _id : ObjectId(hotelId)
-        }, function(err, doc) {
+            _id: ObjectId(hotelId)
+        }, function (err, doc) {
             res.status(200).json(doc);
         });
 };
 
 module.exports.hotelsAddOne = function (req, res) {
+    var db = dbconn.get();
+    var collection = db.collection('hotels');
+    var newHotel;
+
     console.log("POST new hotel");
-    console.log(req.body);
-    res
-        .status(200)
-        .json(req.body);
+
+    if (req.body && req.body.name && req.body.stars) {
+        newHotel = req.body;
+        newHotel.stars = parseInt(req.body.stars, 10);
+        console.log(newHotel);
+        collection.insertOne(newHotel, function(err, response) {
+            console.log(response);
+            console.log(response.ops);
+            res.status(201).json(response.ops);
+        });
+    } else {
+        console.log("Data missing from body");
+        res.status(400).json({ message: "Data missing from body" });
+    }
 };
