659f10c0aacb37857f2a7f62fe6f6fa468aa3555
Wilson Guimarães <wegneto@gmail.com>
2017-04-25 21:35:55 +0100

    Lecture 28: APIs - Creating GET routes using Mongoose routes

--- MEAN/api/controllers/hotels.controllers.js
+++ MEAN/api/controllers/hotels.controllers.js
@@ -1,10 +1,7 @@
-var dbconn = require('../data/dbconnection.js');
-var ObjectId = require('mongodb').ObjectId;
+var mongoose = require('mongoose');
+var Hotel = mongoose.model('Hotel');
 
 module.exports.hotelsGetAll = function (req, res) {
-    var db = dbconn.get();
-    var collection = db.collection('hotels');
-
     var offset = 0;
     var count = 5;
 
@@ -16,29 +13,19 @@ module.exports.hotelsGetAll = function (req, res) {
         count = parseInt(req.query.count, 10);
     }
 
-    collection
-        .find()
-        .skip(offset)
-        .limit(count)
-        .toArray(function (err, docs) {
-            console.log("Found hotels", docs);
-            res.status(200).json(docs);
-        });
+    Hotel.find().skip(offset).limit(count).exec(function (err, hotels) {
+        console.log("Found hotels", hotels.length);
+        res.json(hotels);
+    });
 };
 
 module.exports.hotelsGetOne = function (req, res) {
-    var db = dbconn.get();
-    var collection = db.collection('hotels');
-
     var hotelId = req.params.hotelId;
     console.log("GET hotelID", hotelId);
 
-    collection
-        .findOne({
-            _id: ObjectId(hotelId)
-        }, function (err, doc) {
-            res.status(200).json(doc);
-        });
+    Hotel.findById(hotelId).exec(function (err, doc) {
+        res.status(200).json(doc);
+    });
 };
 
 module.exports.hotelsAddOne = function (req, res) {
@@ -52,7 +39,7 @@ module.exports.hotelsAddOne = function (req, res) {
         newHotel = req.body;
         newHotel.stars = parseInt(req.body.stars, 10);
         console.log(newHotel);
-        collection.insertOne(newHotel, function(err, response) {
+        collection.insertOne(newHotel, function (err, response) {
             console.log(response);
             console.log(response.ops);
             res.status(201).json(response.ops);
