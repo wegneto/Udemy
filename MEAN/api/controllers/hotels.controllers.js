a8064eb178d5e2fc70e2f45a6d44e0c0d3699329
Wilson Guimarães <wegneto@gmail.com>
2017-04-25 22:50:51 +0100

    Lecture 30: APIs - Finding documents based on geo-coordinates

--- MEAN/api/controllers/hotels.controllers.js
+++ MEAN/api/controllers/hotels.controllers.js
@@ -1,9 +1,37 @@
 var mongoose = require('mongoose');
 var Hotel = mongoose.model('Hotel');
 
+var runGeoQuery = function(req, res) {
+    var lat = parseFloat(req.query.lat);
+    var lng = parseFloat(req.query.lng);
+
+    //A geoJSON point
+    var point = {
+        type : "Point",
+        coordinates : [lng, lat]
+    };
+
+    var geoOptions = { 
+        spherical : true,
+        maxDistance : 2000,
+        num : 5
+    };
+
+    Hotel.geoNear(point, geoOptions, function(err, results, stats) {
+        console.log("Geo results", results);
+        console.log("Geo stats", stats);
+        res.status(200).json(results);
+    });
+}
+
 module.exports.hotelsGetAll = function (req, res) {
     var offset = 0;
     var count = 5;
+
+    if (req.query && req.query.lat && req.query.lng) {
+        runGeoQuery(req, res);
+        return;
+    }
 
     if (req.query && req.query.offset) {
         offset = parseInt(req.query.offset, 10);
