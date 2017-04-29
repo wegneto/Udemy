a8430eb0219b4bf0caa9ec74492d772931e3efb7
Wilson Guimarães <wegneto@gmail.com>
2017-04-29 13:05:33 +0100

    Section 4, Lecture 32: Working with Templates and Styles

--- angular2_nodejs/seed-project/routes/app.js
+++ angular2_nodejs/seed-project/routes/app.js
@@ -1,27 +1,8 @@
 var express = require('express');
 var router = express.Router();
-var User = require('../model/User');
 
 router.get('/', function (req, res, next) {
-    User.findOne({}, function (err, doc) {
-        if (err) {
-            return res.send('Error!');
-        } else {
-            res.render('node', {email: doc.email});
-        }
-    });
-});
-
-router.post('/', function (req, res, next) {
-    var email = req.body.email;
-    var user = new User({
-        firstName: 'Wilson',
-        lastName: 'Guimaraes',
-        password: '123456',
-        email: email
-    });
-    user.save();
-    res.redirect('/');
+    res.render('index');
 });
 
 module.exports = router;
