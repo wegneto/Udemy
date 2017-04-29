74815acdcd928f0a6c78c84d3decf9e79d2e8771
Wilson Guimarães <wegneto@gmail.com>
2017-04-29 20:41:34 +0100

    Section 4: Creating the Frontend with Angular 2

--- angular2_nodejs/seed-project/assets/app/app.module.ts
+++ angular2_nodejs/seed-project/assets/app/app.module.ts
@@ -2,15 +2,32 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from "./app.component";
-import {FormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {MessageComponent} from "./messages/message.component";
+import {MessageListComponent} from "./messages/message-list.component";
+import {MessageInputComponent} from "./messages/message-input.component";
+import {MessagesComponent} from "./messages/messages.component";
+import {AuthenticationComponent} from "./auth/authentication.component";
+import {HeaderComponent} from "./header.component";
+import {routing} from "./app.routing";
+import {LogoutComponent} from "./auth/logout.component";
+import {SignupComponent} from "./auth/signup.component";
+import {SigninComponent} from "./auth/signin.component";
 
 @NgModule({
     declarations: [
         AppComponent,
-        MessageComponent
+        MessageComponent,
+        MessageListComponent,
+        MessageInputComponent,
+        MessagesComponent,
+        AuthenticationComponent,
+        HeaderComponent,
+        LogoutComponent,
+        SignupComponent,
+        SigninComponent
     ],
-    imports: [BrowserModule, FormsModule],
+    imports: [BrowserModule, FormsModule, routing, ReactiveFormsModule],
     bootstrap: [AppComponent]
 })
 export class AppModule {
--- angular2_nodejs/seed-project/assets/app/messages/message.component.ts
+++ angular2_nodejs/seed-project/assets/app/messages/message.component.ts
@@ -3,6 +3,7 @@
  */
 import {Component, EventEmitter, Input, Output} from "@angular/core";
 import {Message} from "./message.model";
+import {MessageService} from "./message.service";
 
 @Component({
     selector: 'app-message',
@@ -26,7 +27,13 @@ export class MessageComponent {
     @Input() message: Message;
     @Output() editClicked = new EventEmitter<string>();
 
+    constructor(private messageService: MessageService) {}
+
     onEdit() {
         this.editClicked.emit('A new value');
+    }
+
+    onDelete() {
+        this.messageService.deleteMessage(this.message);
     }
 }
