0389d9601bfbb13bccba63321daeab0f2bdec650
Wilson Guimarães <wegneto@gmail.com>
2017-05-07 21:52:19 +0100

    Lecture 46: AngularJS - Building the SPA - part 3

--- fullstacktraining/MEAN/public/angular-app/app.js
+++ fullstacktraining/MEAN/public/angular-app/app.js
@@ -1,11 +1,17 @@
 angular.module('meanhotel', ['ngRoute'])
 .config(config);
 
-function config($routeProvider) {
+function config($routeProvider, $locationProvider) {
+    $locationProvider.hashPrefix('');
     $routeProvider
         .when('/', {
             templateUrl: 'angular-app/hotel-list/hotels.html',
             controller: HotelsController,
+            controllerAs: 'vm'
+        })
+        .when('/hotel/:id', {
+            templateUrl: 'angular-app/hotel-display/hotel.html',
+            controller: HotelController,
             controllerAs: 'vm'
         });
 }
--- fullstacktraining/MEAN/public/angular-app/hotel-list/hotel-list-controller.js
+++ fullstacktraining/MEAN/public/angular-app/hotel-list/hotel-list-controller.js
@@ -1,10 +1,10 @@
 angular.module('meanhotel').controller('HotelsController', HotelsController);
 
-function HotelsController($http) {
+function HotelsController(hotelDataFactory) {
     var vm = this;
     vm.title = 'MEAN Hotel App';
 
-    $http.get('/api/hotels?count=10').then(function(response) {
+    hotelDataFactory.hotelList().then(function(response) {
         vm.hotels = response.data;
     });
 
