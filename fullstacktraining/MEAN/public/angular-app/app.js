c602e1b1635483603dbaf8d631c8ffdde0de86a2
Wilson Guimarães <wegneto@gmail.com>
2017-06-10 22:51:55 +0100

    Lecture 51. Authentication: Adding authentication to AngularJS

--- fullstacktraining/MEAN/public/angular-app/app.js
+++ fullstacktraining/MEAN/public/angular-app/app.js
@@ -13,6 +13,11 @@ function config($routeProvider, $locationProvider) {
             templateUrl: 'angular-app/hotel-display/hotel.html',
             controller: HotelController,
             controllerAs: 'vm'
+        })
+        .when('/register', {
+            templateUrl: 'angular-app/register/register.html',
+            controller: RegisterController,
+            controllerAs: 'vm'
         });
 }
 
