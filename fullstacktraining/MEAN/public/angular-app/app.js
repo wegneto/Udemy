3e94f3b3960c4fe7867dce58a7006cc07b1e15bf
Wilson Guimarães <wegneto@gmail.com>
2017-07-01 20:24:23 +0100

    [wip] Lecture 52

--- fullstacktraining/MEAN/public/angular-app/app.js
+++ fullstacktraining/MEAN/public/angular-app/app.js
@@ -1,26 +1,46 @@
-angular.module('meanhotel', ['ngRoute'])
-.config(config);
+angular.module('meanhotel', ['ngRoute']).config(config).run(run);
 
-function config($routeProvider, $locationProvider) {
+function config($httpProvider, $routeProvider, $locationProvider) {
+    $httpProvider.interceptors.push('AuthInterceptor');
+
     $locationProvider.hashPrefix('');
     $routeProvider
         .when('/', {
-            templateUrl: 'angular-app/main/main.html'
+            templateUrl: 'angular-app/main/main.html',
+            access: {
+                restricted: false
+            }
         })
         .when('/hotels', {
             templateUrl: 'angular-app/hotel-list/hotels.html',
             controller: HotelsController,
-            controllerAs: 'vm'
+            controllerAs: 'vm',
+            access: {
+                restricted: false
+            }
         })
         .when('/hotel/:id', {
             templateUrl: 'angular-app/hotel-display/hotel.html',
             controller: HotelController,
-            controllerAs: 'vm'
+            controllerAs: 'vm',
+            access: {
+                restricted: false
+            }
         })
         .when('/register', {
             templateUrl: 'angular-app/register/register.html',
             controller: RegisterController,
-            controllerAs: 'vm'
+            controllerAs: 'vm',
+            access: {
+                restricted: false
+            }
+        })
+        .when('/profile', {
+            templateUrl: 'angular-app/profile/profile.html',
+            controllerAs: 'vm',
+            access: {
+                restricted: true
+            }
         })
         .otherwise({
             redirectTo: '/'
